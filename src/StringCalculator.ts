bd2a3a316e9983ba009122d5f4029deb062b2434
ronakpatel1107 <104769863+ronakpatel1107@users.noreply.github.com>
2024-09-13 23:35:25 +0530

    should handle custom delimiters : passed

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -9,7 +9,22 @@ export class StringCalculator{
 
     public add (){
         if (this.input === '') return 0
-        const numberArray = this.input.split(/[\n,]/);
-        return numberArray.reduce((sum, num) => sum + parseInt(num), 0);
+        const numberArray = this.splitBydelimiter(this.input);
+        return numberArray.reduce((sum, num) => sum + num, 0);
+    }
+
+    protected splitBydelimiter(number:string): number[] {
+        
+        let delimiter = /[\n,;]/; 
+        if (number.startsWith('//')) {
+            const delimiterEndIndex = number.indexOf('\n');
+            delimiter = new RegExp(number.substring(2, delimiterEndIndex).trim(), 'g');
+            number = number.substring(delimiterEndIndex + 1);
+        }
+
+        return number
+            .split(delimiter)
+            .map(num => parseInt(num, 10))
+            .filter(num => !isNaN(num));
     }
 }
