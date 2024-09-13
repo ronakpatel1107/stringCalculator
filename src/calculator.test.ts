29949696bd626ba0bda35fb773caf6709129c4dd
ronakpatel1107 <104769863+ronakpatel1107@users.noreply.github.com>
2024-09-13 22:40:15 +0530

    Input not defined

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -3,6 +3,6 @@ import { StringCalculator } from './StringCalculator'
 describe('StringCalculator',()=>{
     
     test('input not defined', () => {
-        expect(new StringCalculator('').add()).toThrow('Error'); 
+        expect(new StringCalculator().add()).toBe(0); 
       });
 })
