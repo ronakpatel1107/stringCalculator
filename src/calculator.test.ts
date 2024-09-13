cef6a3589ab25a54c515d253a8685f1ecdfc3a48
ronakpatel1107 <104769863+ronakpatel1107@users.noreply.github.com>
2024-09-13 22:35:42 +0530

    create a StringCalculator file

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -1,6 +1,6 @@
+import { StringCalculator } from './StringCalculator'
 
 describe('StringCalculator',()=>{
-    
     test('Class not defined', () => {
         expect(new StringCalculator('').add()).toThrow('Error'); 
       });
